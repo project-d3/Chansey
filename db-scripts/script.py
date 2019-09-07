68ddaee75737338de254f8af2196336ce551f8b4
dhanvee <dhanvee@umd.edu>
2019-09-07 19:31:42 -0400

    add building adder and building jsons

--- db-scripts/script.py
+++ db-scripts/script.py
@@ -1,5 +1,8 @@
 from app import db
 from app.models import User, Building, School, Symptom, Report
+import json
+
+#==================================================================================
 
 def make_schools():
     schools = [
@@ -13,27 +16,35 @@ def make_schools():
         db.session.add(s)
         db.session.commit()
 
+#==================================================================================
+
 def make_buildings():
-    school_id = School.query.filter_by(name=data['school']).first().id
 
-    if not school_id:
-        return {
-            'message': 'School not found in database.',
-            'status': False
-        }
-    
-    new_building = Building(name=data['name'], school_id=school_id)
-    try:
-        db.session.add(new_building)
-        db.session.commit()
+    for file in ["UMDBuildings.json", "UCLABuildings.json", "UPennBuildings.json"]:
+        with open(file) as json_source:
+            data = json.loads(json_source.read())
+        
+        school_id = School.query.filter_by(name=data['school']).first().id
 
+        for building in data['buildings']:
+            b = Building(name=building, school_id=school_id)
+            db.session.add(b)
+            db.session.commit()
+
+
+#==================================================================================
 
 def make_users():
 	users = {
 		"Dhanvee Ivaturi": "dhanvee@umd.edu",
 		"Henry Trinh": "dhanvee@umd.edu",
-		"Nikolay Pomytkin": "dhanvee@umd.edu",
+		"Nikolay Pomytkin": "pomytkin@umd.edu",
 		"Ryan Mao": "dhanvee@umd.edu",
+        "User One": "user1@test.com"
+        "User Two": "user2test.com"
+        "User Three": "user3@test.com"
+        "User Four": "user4@test.com"
+        "User Five": "user5@test.com"
 	}
 
     for name, email in users.items():
