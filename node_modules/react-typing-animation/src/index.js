1786cf8442dd4dd8d0f88dff1774593492ccc2ac
thenry3 <43651306+thenry3@users.noreply.github.com>
2019-09-08 03:21:04 -0400

    removed node modules

--- node_modules/react-typing-animation/src/index.js
+++
@@ -1,8 +0,0 @@
-import Typing from './Typing';
-
-export default Typing;
-export { default as Backspace } from './Backspace';
-export { default as Cursor } from './Cursor';
-export { default as Delay } from './Delay';
-export { default as Reset } from './Reset';
-export { default as Speed } from './Speed';
